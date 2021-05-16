a8978a6e69c94745964f26dda4d9ad3753917a7a
“Smith <smith.kakar@gmail.com>
2021-05-16 14:24:38 -0500

    hash table implementation

--- data-structures/sorting-searching/hash_table.py
+++ data-structures/sorting-searching/hash_table.py
@@ -1,1 +1,70 @@
 #! usr/local/bin/env python3
+class HashTable(object):
+    def __init__(self, size):
+
+        self.size = size
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        # we are only using integer keys for ease of use
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            # find the next available slot
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data
+
+    def hashfunction(self, key, size):
+        # remainder method
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash+1) % size
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+
+        position = startslot
+
+        while self.slots[position] != None and not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+
+h = HashTable(5)
+h[1] = 'One'
+h[2] = 'Two'
+h[3] = 'Three'
+print(f'h[1]: {h[1]}')
+h[1] = 'One Updated'
+print(f'h[1]: {h[1]}')
